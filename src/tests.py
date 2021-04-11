2878dfb3bc471ac5983fbf63f3b981b7c1f0dc65
jvuorine <73038013+jvuorine@users.noreply.github.com>
2021-04-11 21:23:41 +0300

    added comments

--- src/tests.py
+++ src/tests.py
@@ -28,6 +28,7 @@ def client():
     os.close(db_fd)
     os.unlink(db_fname)
 
+# function to populate the database
 def _populate_db():
     pr = Project(name="projekti1",
                 status=status_type.NOT_STARTED)
@@ -80,23 +81,28 @@ def _populate_db():
     db.session.add(ta3)
     db.session.commit()
 
+# get valid json for member
 def _get_member_json(number=1):    
     return {"name": f"extra-member-{number}"}
 
+# get valid json for project
 def _get_project_json(number=1):
     return {"name": f"extra-project-{number}",
             "status": "NOT_STARTED"
             }
 
+# test to check the namespace
 def _check_namespace(client, response):
     namespace = response["@namespaces"]["promana"]["name"]
     resp = client.get(namespace)
     assert resp.status_code == 200
 
+# test project collection
 class TestProjectCollection(object):
 
     RESOURCE_URL = "/api/projects/"
 
+    # test for get request
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -106,6 +112,7 @@ class TestProjectCollection(object):
             assert "name" in item
             assert "status" in item
 
+    # test post request
     def test_post(self, client):
         valid = _get_project_json()
 
@@ -132,12 +139,14 @@ class TestProjectCollection(object):
         resp = client.post(self.RESOURCE_URL, json=invalid)
         assert resp.status_code == 400
 
+# test project item
 class TestProjectItem(object):
 
     RESOURCE_URL = "/api/projects/test-project-1/"
     INVALID_URL = "/api/projects/non-project-x/"
     MODIFIED_URL = "/api/projects/extra-project-1/"
 
+    # test get request
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -147,6 +156,7 @@ class TestProjectItem(object):
         resp = client.get(self.INVALID_URL)
         assert resp.status_code == 404
 
+    # test put request
     def test_put(self, client):
         valid = _get_project_json()
 
@@ -154,6 +164,7 @@ class TestProjectItem(object):
         resp = client.put(self.RESOURCE_URL, data=json.dumps(valid))
         assert resp.status_code == 415
 
+        # test with invalid URL
         resp = client.put(self.INVALID_URL, json=valid)
         assert resp.status_code == 404
 
@@ -172,6 +183,7 @@ class TestProjectItem(object):
         resp = client.put(self.RESOURCE_URL, json=invalid)
         assert resp.status_code == 400
 
+        # test if the modification modified the name
         valid = _get_project_json()
         resp = client.put(self.RESOURCE_URL, json=valid)
         resp = client.get(self.MODIFIED_URL)
@@ -179,18 +191,24 @@ class TestProjectItem(object):
         body = json.loads(resp.data)
         assert body["name"] == valid["name"]
 
+    # test delete
     def test_delete(self, client):
+        # test for valid url
         resp = client.delete(self.RESOURCE_URL)
         assert resp.status_code == 204
+        # test the same url again
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 404
+        # test with invalid url
         resp = client.delete(self.INVALID_URL)
         assert resp.status_code == 404
 
+# test member collection
 class TestMemberCollection(object):
 
     RESOURCE_URL = "/api/members/"
 
+    # test get request
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -199,6 +217,7 @@ class TestMemberCollection(object):
         for item in body["items"]:
             assert "name" in item
 
+    # test post
     def test_post(self, client):
         valid = _get_member_json()
         
@@ -224,12 +243,14 @@ class TestMemberCollection(object):
         resp = client.post(self.RESOURCE_URL, json=invalid)
         assert resp.status_code == 400
 
+# test member item
 class TestMemberItem(object):
 
     RESOURCE_URL = "/api/members/test-member-1/"
     INVALID_URL = "/api/members/non-member-x/"
     MODIFIED_URL = "/api/members/extra-member-1/"
 
+    # test get
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -238,6 +259,7 @@ class TestMemberItem(object):
         resp = client.get(self.INVALID_URL)
         assert resp.status_code == 404
 
+    # test patch
     def test_patch(self, client):       
         valid = _get_member_json()
         
@@ -245,6 +267,7 @@ class TestMemberItem(object):
         resp = client.patch(self.RESOURCE_URL, data=json.dumps(valid))
         assert resp.status_code == 415
         
+        # test with invalid url
         resp = client.patch(self.INVALID_URL, json=valid)
         assert resp.status_code == 404
         
@@ -263,6 +286,7 @@ class TestMemberItem(object):
         resp = client.patch(self.RESOURCE_URL, json=invalid)
         assert resp.status_code == 400
         
+        # test if the name changed
         valid = _get_member_json()
         resp = client.patch(self.RESOURCE_URL, json=valid)
         resp = client.get(self.MODIFIED_URL)
@@ -270,18 +294,24 @@ class TestMemberItem(object):
         body = json.loads(resp.data)
         assert body["name"] == valid["name"]
     
+    # test delete
     def test_delete(self, client):
+        # test with valid url
         resp = client.delete(self.RESOURCE_URL)
         assert resp.status_code == 204
+        # test with same url again
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 404
+        # test with invalid url
         resp = client.delete(self.INVALID_URL)
         assert resp.status_code == 404
 
+# test project members
 class TestProjectMemberCollection(object):
 
     RESOURCE_URL = "/api/projects/projekti1/members/"
 
+    # test get
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -290,11 +320,13 @@ class TestProjectMemberCollection(object):
         for item in body["items"]:
             assert "name" in item
 
+# test project member
 class TestProjectMemberItem(object):
     RESOURCE_URL = "/api/projects/projekti1/members/test-member-1/"
     INVALID_URL = "/api/projects/projekti1/members/test-member-x/"
     INVALID_URL2 = "/api/projects/projekti1/members/test-member-4/"
 
+    # test delete
     def test_delete(self, client):
         # delete existing member from project
         resp = client.delete(self.RESOURCE_URL)
@@ -306,11 +338,13 @@ class TestProjectMemberItem(object):
         resp = client.delete(self.INVALID_URL2)
         assert resp.status_code == 404
 
+# test task members
 class TestTaskMemberCollection(object):
 
     RESOURCE_URL = "/api/projects/projekti1/phases/phase1/tasks/task1/members/"
     RESOURCE_URL2 = "/api/projects/projekti1/phases/phase1/tasks/task3/members/"
 
+    # test get
     def test_get(self, client):
         resp = client.get(self.RESOURCE_URL)
         assert resp.status_code == 200
@@ -319,6 +353,7 @@ class TestTaskMemberCollection(object):
         for item in body["items"]:
             assert "name" in item
 
+    # test post
     def test_post(self, client):
         valid = {"name": "test-member-1"}
         
@@ -340,11 +375,13 @@ class TestTaskMemberCollection(object):
         resp = client.post(self.RESOURCE_URL2, json=invalid)
         assert resp.status_code == 400
 
+# test task member
 class TestTaskMemberItem(object):
     RESOURCE_URL = "/api/projects/projekti1/phases/phase1/tasks/task1/members/test-member-1/"
     INVALID_URL = "/api/projects/projekti1/phases/phase1/tasks/task1/members/test-member-x/"
     INVALID_URL2 = "/api/projects/projekti2/phases/phase1/tasks/task1/members/test-member-4/"
 
+    # test delete
     def test_delete(self, client):
         # delete existing member from project
         resp = client.delete(self.RESOURCE_URL)
